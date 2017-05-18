ed3e511d236392a65c809bc62e657e5984dc0b0f
P. Gomersbach <p.gomersbach@rely.nl>
2017-05-18 15:42:50 +0000

    add telegraf metrics collector

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,8 @@ class role_appl
   include ::profile_base
   # include rspec monitor to make rspec acceptance test available to monitor system
   include ::profile_base::rspec_monitor
+  include ::profile_telegraf
+  include ::profile_telegraf::rspec_monitor
   include ::profile_apache
   include ::profile_apache::rspec_monitor
 }
